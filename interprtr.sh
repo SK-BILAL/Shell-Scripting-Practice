922605d7af2626086e5c9d499e22e1610a9a6ca7
Bilal <skbtrades9994@gmail.com>
2024-11-25 02:47:15 +0530

    first commit file

---
+++ interprtr.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+########################
+# Name: Bilal
+# Date: 25/11/2024
+# This is an example to show the purpose of writing Shebang in a Shell Script file 
+# Shebang indicates the interpreter and its PATH, that this file is run by which interpreter. 
+#########################
+print(" this is the example of shebang for python.")
