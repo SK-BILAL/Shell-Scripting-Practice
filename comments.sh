5a618874b83b99f116cf87dd1cf86cd7507ffaf7
SK-BILAL <skbtrades9994@gmail.com>
2024-11-28 00:57:45 +0530

    example of comments in shell scripting

---
+++ comments.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#Name : Bilal
+#Date: 28/11/24
+#
+#this is a sample shell script to demonstrate how comments work in shell scripting
+#
+####################
+
+echo "This is an example of comment"
+
+echo "
+my 
+name 
+is 
+Bilal
+"
+
+echo "my \
+name \
+is\
+"
+
+echo -e "this is \t an \t example of tab"
+
+echo -e  "this is \n example of \n slash n"
